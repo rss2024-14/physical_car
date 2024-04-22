0fd28b4ca97f4ed980cf9a5c53ddc1d1428fad76
Kristoff Misquitta <kmisqui@mit.edu>
2024-04-22 01:45:16 +0000

    progress in following single point

--- path_planning/path_planning/trajectory_follower.py
+++ path_planning/path_planning/trajectory_follower.py
@@ -30,6 +30,8 @@ class PurePursuit(Node):
         self.wheelbase_length = .3  # FILL IN #
         self.initialized_traj = False
 
+        self.index_in_path = None
+
         self.trajectory = LineTrajectory("/followed_trajectory")
 
         self.odom_sub = self.create_subscription(Odometry,
@@ -59,48 +61,32 @@ class PurePursuit(Node):
             odometry_msg.pose.pose.orientation.z,
             odometry_msg.pose.pose.orientation.w
         ]
-        mat = tf_transformations.quaternion_matrix(q)
-        z_rot = np.arctan2(mat[1, 0], mat[0, 0])
-        self.heading = z_rot
+        *_, theta = tf_transformations.euler_from_quaternion([q[0], q[1], q[2], q[3]])
+
+        # mat = tf_transformations.quaternion_matrix(q)
+        # z_rot = np.arctan2(mat[1, 0], mat[0, 0])
+        heading = theta
         
         pts = self.trajectory.points
-        closest_pts = self.find_closest_points(pts, robot_xy)
+        if self.index_in_path is None:
+            self.index_in_path = self.find_closest(pts, robot_xy)
 
-        # Linear regression
-        # x_coords = list( map(lambda pt: pt[0], closest_pts) )
-        # y_coords = list( map(lambda pt: pt[1], closest_pts) )
+        # self.get_logger().info("path index %s" % (self.index_in_path,))
 
-        m = ( closest_pts[1][1] - closest_pts[0][1] ) / (closest_pts[1][0] - closest_pts[0][0])
-        b = -m * closest_pts[0][0] + closest_pts[0][1]
+        target = self.trajectory.points[self.index_in_path]
+        if self.distance(target, robot_xy) < self.lookahead:
+            self.index_in_path += 1
+            self.index_in_path = min(self.index_in_path, len(self.trajectory.points)-1)
 
-        self.get_logger().info("robot_xy %s" % (robot_xy,))
-        # self.get_logger().info("x_coords %s" % (x_coords,))
-        self.get_logger().info("closest_pts %s" % (closest_pts,))
-
-        # lin_reg = np.polyfit(x_coords, y_coords, 1)
-        # m = lin_reg[0]
-        # b = lin_reg[1]
-        sample_pts = np.arange(-5, 1, .04)
-        y = m * sample_pts + b
-        self.get_logger().info("closest points %s" % (closest_pts,))
-        self.get_logger().info("line %s %s" % (m, b))
-        # self.plot_line(sample_pts, y, self.line_pub, color=(0.,0.796,1.))
-        self.plot_line([closest_pts[0][0], closest_pts[1][0]], [closest_pts[0][1], closest_pts[1][1]], self.line_pub, color=(0.,0.796,1.))
-        # Pure pursuit
-        perp_dist = np.abs(b) / np.sqrt(m**2 + 1)
-
-        if (self.lookahead <= perp_dist):
-            angle_robot_to_intersect = 0
-        else:
-            angle_robot_to_intersect = np.pi/2 - np.arccos(perp_dist / self.lookahead)
+        target_in_robot_frame = np.linalg.inv( self.tf_matrix(theta, robot_xy[0], robot_xy[1]) ) @ np.array([[target[0]], [target[1]], [1]])
         
-        if (b < 0):
-            angle_robot_to_intersect *= -1
+        # self.get_logger().info("target x in RF %s, target y in RF %s" % (target_in_robot_frame[0], target_in_robot_frame[1]))
 
-        eta = -self.heading + angle_robot_to_intersect
-        delta = np.arctan(2 * self.wheelbase_length * np.sin(eta) / self.lookahead)
-        
-        drive_cmd = self.build_drive_cmd(delta)
+        target_rotation = np.arctan2(target_in_robot_frame[1], target_in_robot_frame[0])[0]
+        self.get_logger().info("target rotation %s" % (target_rotation*180/np.pi,))
+        self.get_logger().info("heading %s" % (heading*180/np.pi,))
+        # self.get_logger().info("steering angle %s" % ( (heading - target_rotation)*180/np.pi,))
+        drive_cmd = self.build_drive_cmd(heading - target_rotation)
         self.drive_pub.publish(drive_cmd)
 
     def trajectory_callback(self, msg):
@@ -115,8 +101,10 @@ class PurePursuit(Node):
     def distance(self, p1, p2):
         return math.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
 
-    def find_closest_points(self, pts, ref_pt):
-        return sorted(pts, key=lambda p: self.distance(p, ref_pt))[:2]
+    def find_closest(self, pts, ref_pt):
+        distances = [self.distance(pt, ref_pt) for pt in pts]
+        min_distance = min(distances)
+        return distances.index(min_distance)
     
     def build_drive_cmd(self, delta):
         drive_cmd = AckermannDriveStamped()
@@ -125,6 +113,14 @@ class PurePursuit(Node):
         drive_cmd.drive.speed = self.speed
         drive_cmd.drive.steering_angle = delta
         return drive_cmd
+    
+    def tf_matrix(self, angle, x, y):
+        cos_theta = np.cos(angle)
+        sin_theta = np.sin(angle)
+        
+        return np.array([[cos_theta, -sin_theta, x],
+                        [sin_theta,  cos_theta, y],
+                        [0, 0, 1]])
     
     def plot_line(self, x, y, publisher, color = (1., 0., 0.), frame = "/base_link"):
         """
